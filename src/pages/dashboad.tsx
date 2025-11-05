37eeaff26054c73c5764692b2b9d08fb37ab2a2b
Devmodjo <yvankamsu88@gmail.com>
2025-11-05 12:25:48 +0100

    (admin) initializ dashboard component

---
+++ src/pages/dashboad.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from "@/components/layout/layout";
+
+export default function Dashboard() {
+  return (
+    <Layout 
+    title='DashBoard administrateur - Gestion des projets & Blog'
+    description='Interface dashboard'>
+      <section>
+        
+      </section>
+    </Layout>
+  )
+}
