be44623b205a196f7cebdc851563bab88bd7d28e
Wistant Kode <wistantkode@protonmail.com>
2025-12-03 08:16:53 +0100

    refactor(code): refactor and clean the project code.
    News files, separates const and data....

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ import ContactClient from './ContactClient';
 import {Metacontact} from "@/lib/metaData";
 
 // --- STATIC METADATA ---
-const metadata: Metadata = Metacontact
+export const metadata: Metadata = Metacontact
 
 const ContactPage = () => {
   return <ContactClient />;
