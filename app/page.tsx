5356a8d914766d97c16869c434e8c4948879a1a4
Wistant Kode <wistantkode@protonmail.com>
2025-12-07 05:38:42 +0100

    add FAQ section

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function HomePage() {
       <ServicesSection/>
       <MyStack />
       <MyExperience/>
-      <MyExperience/>
+ 
       <FaqSection />
       <TestimonialsSection />
       {/* <ContactForm/> */}
